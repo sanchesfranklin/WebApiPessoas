48c44f80c252696439c867bcd045ec6a348a46dc
Eduardo Luiz <bal699@hotmail.com>
2021-04-27 10:47:12 -0300

    Adicionado comentários de revisão

--- Scripts SQL/5-Script_Procedure_Inserir.sql
+++ Scripts SQL/5-Script_Procedure_Inserir.sql
@@ -1,5 +1,5 @@
 CREATE PROCEDURE InserirCadastro
-	@idPessoa int = NULL,
+	@idPessoa int = NULL, -- ELG: Percebi que vc não está usando esse parâmetro. Será que é realmente necessário ele estar aqui?
 	@nome VARCHAR(50) = NULL,
 	@dataNascimento DATE = NULL,
 	@salario DECIMAL = NULL,
@@ -14,5 +14,6 @@ BEGIN
 
 	INSERT INTO Pessoas(nome, dataNascimento, salario, observacoes, nomeMae, nomePai, cpf)
 	VALUES(@nome, @dataNascimento, @salario, @observacoes, @nomeMae, @nomePai, @cpf)
-
+  
+  -- ELG: O item 5 pede para vc retornar alguma coisa antes de finalizar a execução da procedure.
 END

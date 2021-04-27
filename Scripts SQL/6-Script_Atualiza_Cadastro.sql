f60a1fd15fa0b9caecd057c25b28022a2cbbca0d
Eduardo Luiz <bal699@hotmail.com>
2021-04-27 10:32:25 -0300

    Adicionado comentários de revisão

--- Scripts SQL/6-Script_Atualiza_Cadastro.sql
+++ Scripts SQL/6-Script_Atualiza_Cadastro.sql
@@ -7,12 +7,12 @@ CREATE PROCEDURE AtualizaCadastro
 	@nomeMae VARCHAR(50) = NULL,
 	@nomePai VARCHAR(50) = NULL,
 	@cpf CHAR(11) = NULL
-
+  -- ELG: Ateção para formatação e padronização do código. Remova a linha em branco. 
 AS
 BEGIN
 
 	UPDATE Pessoas
-	SET nome = @nome, dataNascimento = @dataNascimento, salario = @salario, observacoes = @observacoes, 
+	SET nome = @nome, dataNascimento = @dataNascimento, salario = @salario, observacoes = @observacoes, -- ELG: Colocar um campo por linha, melhora a legibilidade do código
 	nomeMae = @nomeMae, nomePai = @nomePai, cpf = @cpf
 	WHERE idPessoa = @idPessoa
 

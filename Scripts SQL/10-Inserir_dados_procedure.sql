bcfe94bb9483f5f5945ecd48909d989532a4eaca
Eduardo Luiz <bal699@hotmail.com>
2021-04-27 10:34:01 -0300

    Adicionado comentários de revisão

--- Scripts SQL/10-Inserir_dados_procedure.sql
+++ Scripts SQL/10-Inserir_dados_procedure.sql
@@ -1,1 +1,1 @@
-EXEC InserirCadastro null,'Sanches','1990-11-11',3500,'Inserido via procedure','Lucia','Josias',09239857435
+EXEC InserirCadastro null,'Sanches','1990-11-11',3500,'Inserido via procedure','Lucia','Josias',09239857435 -- ELG: O primeiro parametro null nunca é utilizado pela procedure, então vc pode remover
